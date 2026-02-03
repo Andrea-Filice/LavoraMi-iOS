3637b2e633cd9ae1bdceee2aca18aa1494af9066
Andrea Filice <filiceandrea8@gmail.com>
2026-02-03 15:27:31 +0100

    chore: removed old keys

--- LavoraMi/SupabaseAuth/SupabaseClient.swift
+++ LavoraMi/SupabaseAuth/SupabaseClient.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Supabase
 
-let supabaseUrl = URL(string: "https://nfwcqsdbniwmmwcljgnw.supabase.co")!
-let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5md2Nxc2Ribml3bW13Y2xqZ253Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MDc4OTUsImV4cCI6MjA4NDQ4Mzg5NX0.n0BOksrd2mg4a0yVEuxFXTLu_aRCi-JWW7SXHrcGbDI"
+let supabaseUrl = URL(string: "")!
+let supabaseKey = ""
 
 let supabase = SupabaseClient(supabaseURL: supabaseUrl, supabaseKey: supabaseKey)
